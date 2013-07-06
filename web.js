9bfef6f767cd4841f81dc58ff40b965991a6ccf2
xircst2 <xircst2@mailinator.com>
2013-07-06 15:43:50 +0000

    update

--- web.js
+++ web.js
@@ -1,14 +1,23 @@
+#!/usr/bin/env node
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//var data = fs.readFileSync('index.html', {'flag':'r'});
+var data = '';
+var rs = fs.createReadStream('index.html',{encoding:'utf-8'});
+rs.on('data',function(trunk) {
+  data += trunk.toString('utf8');
 });
 
-var port = process.env.PORT || 5000;
+app.get('/', function(request, response) {
+  response.send(data);
+});
+
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
